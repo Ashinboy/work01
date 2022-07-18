cb37c3bedd0fbe5d57ad9862a1e34183fac8bbb1
Ashin <ashinwangboy@gmail.com>
2022-07-18 22:00:59 +0800

    Initial Commit

---
+++ work01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  work01
+//
+//  Created by Ashin Wang on 2022/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ work01/work01App.swift
@@ -0,0 +1,17 @@
+//
+//  work01App.swift
+//  work01
+//
+//  Created by Ashin Wang on 2022/7/18.
+//
+
+import SwiftUI
+
+@main
+struct work01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
